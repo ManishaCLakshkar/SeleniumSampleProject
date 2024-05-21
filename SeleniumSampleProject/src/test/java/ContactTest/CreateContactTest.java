62a05488a54570a83f425d1f758ebaadd749388f
NishaSuryachar <157368953+NishaSuryachar@users.noreply.github.com>
2024-05-21 17:44:08 +0530

    comitted newly

--- SeleniumSampleProject/src/test/java/ContactTest/CreateContactTest.java
+++ SeleniumSampleProject/src/test/java/ContactTest/CreateContactTest.java
@@ -6,14 +6,10 @@ public class CreateContactTest {
 	@Test
 	public void createContactTest()
 	{
-		String URL = System.getProperty("url");
-		String BROWSER = System.getProperty("browser");
-		String USERNAME = System.getProperty("username");
-		String PASSWORD = System.getProperty("password");
-		System.out.println(URL);
-		System.out.println(BROWSER);
-		System.out.println(USERNAME);
-		System.out.println(PASSWORD);
+		System.out.println("URL");
+		System.out.println("BROWSER");
+		System.out.println("USERNAME");
+		System.out.println("PASSWORD");
          System.out.println("Execute createContactTest");
 	}
 	
